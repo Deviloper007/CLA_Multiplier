740182da3f3fbc2da7b9ebc925e4a0abbe973841
Yash Chauhan <63181536+Deviloper007@users.noreply.github.com>
2023-07-29 16:19:53 +0530

    Add files via upload

---
+++ cpu_wb_cla_multiplier.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:39:40 07/28/2023 
+// Design Name: 
+// Module Name:    cpu_wb_cla_multiplier 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module cla_multiplier (multicand, multiplier, product);
+parameter MULTICAND_WID = 32;
+parameter MULTIPLIER_WID = 32;
+
+input [MULTICAND_WID-1:0] multicand;
+input [MULTIPLIER_WID-1:0] multiplier;
+output [(MULTICAND_WID + MULTIPLIER_WID - 1):0] product;
+
+wire [MULTICAND_WID - 1:0] multicand_tmp [MULTIPLIER_WID-1:0];
+wire [MULTICAND_WID - 1:0] product_tmp [MULTIPLIER_WID-1:0];
+wire [MULTIPLIER_WID -1:0] carry_tmp;
+ genvar i, j;
+generate 
+ for(j = 0; j < MULTIPLIER_WID; j = j + 1) begin: for_loop_j
+ assign multicand_tmp[j] =  multicand & {MULTICAND_WID{multiplier[j]}};
+ end
+ 
+ assign product_tmp[0] = multicand_tmp[0];
+ assign carry_tmp[0] = 1'b0;
+ assign product[0] = product_tmp[0][0];
+ for(i = 1; i < MULTIPLIER_WID; i = i + 1) begin: for_loop_i
+ cpu_wb_cla_adder #(.DATA_WID(MULTIPLIER_WID)) add1 (
+     // Outputs
+     .sum(product_tmp[i]),
+     .carry_out(carry_tmp[i]),
+     // Inputs
+   .carry_in(1'b0),
+     .in1(multicand_tmp[i]),
+     .in2({carry_tmp[i-1],product_tmp[i-1][31-:31]}));
+ assign product[i] = product_tmp[i][0];
+ end //end for loop
+ assign product[(MULTIPLIER_WID+MULTIPLIER_WID-1):MULTIPLIER_WID] = {carry_tmp[MULTIPLIER_WID-1],product_tmp[MULTIPLIER_WID-1][31-:31]};
+endgenerate
+endmodule
---
+++ mult_tb.v
@@ -0,0 +1,55 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   12:40:05 07/28/2023
+// Design Name:   cpu_wb_cla_multiplier
+// Module Name:   E:/xilinx/location_workingDirectory/cla_multiplier/mult_tb.v
+// Project Name:  cla_multiplier
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: cpu_wb_cla_multiplier
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module mult_tb;
+
+	// Inputs
+	reg [31:0] multicand;
+	reg [31:0] multiplier;
+
+	// Outputs
+	wire [63:0] product;
+
+	// Instantiate the Unit Under Test (UUT)
+	cla_multiplier uut (
+		.multicand(multicand), 
+		.multiplier(multiplier), 
+		.product(product)
+	);
+
+	initial begin
+		// Initialize Inputs
+		multicand = 0;
+		multiplier = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+		  multicand = 32'h00007FFF;
+        multiplier = 32'h0000007F;
+	end
+      
+endmodule
+
